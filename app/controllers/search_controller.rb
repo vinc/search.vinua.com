501538c6d7f065b5cba9b9cb493945e3a4aa52e5
Vincent Ollivier <v@vinc.cc>
2023-04-25 00:28:19 +0200

    Count user queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,21 +4,17 @@ class SearchController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    current_user.increment!(:queries_count)
     expires_in 5.minutes, public: true
-
     @query = search_params[:q].presence
-
     return redirect_to(root_path) if @query.blank?
 
     bang = Bang.new(@query)
-
     if bang.url.present?
       return redirect_to(bang.url, allow_other_host: true)
     end
-
     @title = [@query, "Search"].compact.join(" - ")
     @results = Query.new(@query, language: prefered_languages.first).search
-
     if bang.name == "!"
       res = @results.first
       return redirect_to(res.url, allow_other_host: true) if res
