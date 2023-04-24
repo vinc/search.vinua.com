367b13690c13e4c6a584f6643bf290357d57a189
Vincent Ollivier <v@vinc.cc>
2023-04-24 22:36:09 +0200

    Fix missing crawl date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def time_ago(time)
-    time_tag(time, time_ago_in_words(time), title: time)
+    time_tag(time, time_ago_in_words(time), title: time) if time
   end
 end
